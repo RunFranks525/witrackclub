80feb5cc5be8bf98eba1ea0ab8763c6317e4e382
Scott Franklin <stfranklin@wisc.edu>
2016-03-17 14:02:53 -0500

    Attempting to fix code first migrations with identity columns

--- WisconsinTrackClubWebsite/Migrations/Configuration.cs
+++ WisconsinTrackClubWebsite/Migrations/Configuration.cs
@@ -9,7 +9,8 @@ namespace WisconsinTrackClubWebsite.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(WisconsinTrackClubWebsite.Models.ApplicationDbContext context)
